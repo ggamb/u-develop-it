dd0c6472e8ab0a202d198db1a11f780ff0ae2179
ggamb <glenngambardella372@gmail.com>
2021-10-18 13:02:25 -0400

    working election database

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: process.env.password,
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.use((req, res) => {
     res.status(404).end();
